f98721a1dbf0b6819ff3e8883006fa8d8a719c74
Nihal Huseynzade <nihalhuseynov08@gmail.com>
2025-05-10 10:24:21 +0000

    amdoad

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,24 +3,25 @@
 /**
  * print_line - Draws a straight line according to parameter
  * @n: The number of lines to draw
- *
- * Return: void
+ * Return: empty
  */
+
 void print_line(int n)
+
 {
+
 	int x;
 
 	if (n <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x < n; x++)
-		{
-			_putchar(95);
-		}
-		_putchar('\n');
+	for (x = 0; x < n; x++)
+	{
+	_putchar(95);
 	}
-}
- 
+	_putchar('\n');
+	}
+}               
