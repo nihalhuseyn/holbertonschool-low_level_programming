4f203b69f3abec0b7c0a7c175a1783a48db417dc
Nihal Huseynzade <nihalhuseynov08@gmail.com>
2025-05-10 10:20:37 +0000

    456789

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,24 +3,24 @@
 /**
  * print_line - Draws a straight line according to parameter
  * @n: The number of lines to draw
- * Return: empty
+ *
+ * Return: void
  */
-
 void print_line(int n)
-
 {
 	int x;
 
 	if (n <= 0)
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
-	for (x = 0; x < n; x++)
-	{
-	_putchar(95);
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	}
-}              
+}
+        
